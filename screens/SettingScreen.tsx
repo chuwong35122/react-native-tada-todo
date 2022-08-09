3d9a90804fceea7dc94b634c4c77084c59165387
Trainee_Chuwong Kulrattanarak <trainee_chuwong@tencent.co.th>
2022-08-09 17:51:13 +0700

    fix: setting screen scrollview

--- screens/SettingScreen.tsx
+++ screens/SettingScreen.tsx
@@ -8,6 +8,7 @@ import {
   VStack,
   HStack,
   useToast,
+  ScrollView,
 } from "native-base";
 import React, { useState, useRef, useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -18,13 +19,11 @@ import LanguageSelector from "../components/ui/LanguageSelector";
 import { clearStorage } from "../utils/settings";
 import { useTranslation } from "react-i18next";
 import ToastMessage from "./../components/ui/ToastMessage";
-import { useWindowDimensions } from "react-native";
 
 const SettingScreen = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<StackNavigationScreenTypes>>();
   const { t } = useTranslation();
-  const { height } = useWindowDimensions();
 
   const { updateTodoList } = useContext(TodoContext);
   const [isAlertOpen, setIsAlertOpen] = useState(false);
@@ -55,96 +54,98 @@ const SettingScreen = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {/* Alert confirmation */}
-      <AlertDialog
-        leastDestructiveRef={cancelRef}
-        isOpen={isAlertOpen}
-        onClose={onClose}
-      >
-        <AlertDialog.Content>
-          <AlertDialog.CloseButton />
-          <AlertDialog.Header>{t("clearModal.header")}</AlertDialog.Header>
-          <AlertDialog.Body>{t("clearModal.body")}</AlertDialog.Body>
-          <AlertDialog.Footer>
-            <Button.Group space={4}>
-              <Button
-                variant="unstyled"
-                onPress={onClose}
-                ref={cancelRef}
-                backgroundColor="black"
-                _text={{ color: "white" }}
-              >
-                {t("clearModal.cancelBtn")}
-              </Button>
-              <Button
-                variant="ghost"
-                _text={{ color: "rose.600" }}
-                onPress={onPressDeleteTodo}
-                _pressed={{ backgroundColor: "rose.200" }}
-              >
-                {t("clearModal.deleteBtn")}
-              </Button>
-            </Button.Group>
-          </AlertDialog.Footer>
-        </AlertDialog.Content>
-      </AlertDialog>
-      <VStack px="4" space={3}>
-        <Text fontSize="3xl" fontFamily="Roboto_500Medium">
-          {t("settings.header")}
-        </Text>
-        {/* Settings */}
-        <HStack justifyContent="space-between" alignItems="center">
-          <Text fontSize="lg" fontFamily="Roboto_300Light">
-            {t("settings.languageLabel")}
+      <ScrollView>
+        {/* Alert confirmation */}
+        <AlertDialog
+          leastDestructiveRef={cancelRef}
+          isOpen={isAlertOpen}
+          onClose={onClose}
+        >
+          <AlertDialog.Content>
+            <AlertDialog.CloseButton />
+            <AlertDialog.Header>{t("clearModal.header")}</AlertDialog.Header>
+            <AlertDialog.Body>{t("clearModal.body")}</AlertDialog.Body>
+            <AlertDialog.Footer>
+              <Button.Group space={4}>
+                <Button
+                  variant="unstyled"
+                  onPress={onClose}
+                  ref={cancelRef}
+                  backgroundColor="black"
+                  _text={{ color: "white" }}
+                >
+                  {t("clearModal.cancelBtn")}
+                </Button>
+                <Button
+                  variant="ghost"
+                  _text={{ color: "rose.600" }}
+                  onPress={onPressDeleteTodo}
+                  _pressed={{ backgroundColor: "rose.200" }}
+                >
+                  {t("clearModal.deleteBtn")}
+                </Button>
+              </Button.Group>
+            </AlertDialog.Footer>
+          </AlertDialog.Content>
+        </AlertDialog>
+        <VStack px="4" space={3}>
+          <Text fontSize="3xl" fontFamily="Roboto_500Medium">
+            {t("settings.header")}
           </Text>
-          <LanguageSelector />
-        </HStack>
-        <View alignItems="center" justifyContent="center" px="6">
-          <LottieView
-            autoPlay
-            loop
-            style={{
-              width: "100%",
-              height: height * 0.5,
+          {/* Settings */}
+          <HStack justifyContent="space-between" alignItems="center">
+            <Text fontSize="lg" fontFamily="Roboto_300Light">
+              {t("settings.languageLabel")}
+            </Text>
+            <LanguageSelector />
+          </HStack>
+          <View alignItems="center" justifyContent="center" px="6">
+            <LottieView
+              autoPlay
+              loop
+              style={{
+                width: "100%",
+                height: 300,
+              }}
+              source={require("../assets/lottie/cat.json")}
+            />
+            <Text fontSize="lg" fontFamily="Roboto_300Light" textAlign="center">
+              {t("settings.catText1")}
+            </Text>
+            <Text fontSize="lg" fontFamily="Roboto_300Light" textAlign="center">
+              {t("settings.catText2")}
+            </Text>
+          </View>
+        </VStack>
+        {/* Clear Storage Button */}
+        <VStack space={4} px="4" mt="16" w="full">
+          <Button
+            size="lg"
+            variant="outline"
+            _text={{
+              color: "black",
             }}
-            source={require("../assets/lottie/cat.json")}
-          />
-          <Text fontSize="lg" fontFamily="Roboto_300Light" textAlign="center">
-            {t("settings.catText1")}
-          </Text>
-          <Text fontSize="lg" fontFamily="Roboto_300Light" textAlign="center">
-            {t("settings.catText2")}
-          </Text>
-        </View>
-      </VStack>
-      {/* Clear Storage Button */}
-      <VStack space={4} px="4" position="absolute" bottom="10" w="full">
-        <Button
-          size="lg"
-          variant="outline"
-          _text={{
-            color: "black",
-          }}
-          _pressed={{
-            backgroundColor: "gray.200",
-            borderColor: "gray.300",
-          }}
-          _focus={{
-            backgroundColor: "gray.200",
-          }}
-          onPress={() => setIsAlertOpen(true)}
-        >
-          {t("settings.clearAll")}
-        </Button>
-        <Button
-          size="lg"
-          variant="solid"
-          bgColor="black"
-          onPress={() => navigation.goBack()}
-        >
-          {t("settings.back")}
-        </Button>
-      </VStack>
+            _pressed={{
+              backgroundColor: "gray.200",
+              borderColor: "gray.300",
+            }}
+            _focus={{
+              backgroundColor: "gray.200",
+            }}
+            onPress={() => setIsAlertOpen(true)}
+          >
+            {t("settings.clearAll")}
+          </Button>
+          <Button
+            size="lg"
+            variant="solid"
+            bgColor="black"
+            onPress={() => navigation.goBack()}
+          >
+            {t("settings.back")}
+          </Button>
+        </VStack>
+      </ScrollView>
     </SafeAreaView>
   );
 };
